2c6157efdc2fead53452f8bbd581cfaeaf0e3574
Lars Andersen Bratholm <lab@fend01.cluster>
2018-04-16 00:37:46 +0200

    osprey fix

--- make_config.py
+++ make_config.py
@@ -20,7 +20,7 @@ def write_config():
         d["strategy"] = {"name": STRATEGY, "params": {"seeds": SEEDS}}
     elif KAPPA == None:
         d["strategy"] = {"name": STRATEGY, "params": {"seeds": SEEDS, "acquisition": "{ name : %s, params : {}}" % ACQUISITION, 
-                "n_init": NUM_INIT, "n_iter": NUM_ITER, "sobol_init": SOBOL_INIT}}
+            "n_init": NUM_INIT, "n_iter": NUM_ITER, "sobol_init": SOBOL_INIT, "optimize_best": OPTIMIZE_BEST}}
     else:
         d["strategy"] = {"name": STRATEGY, "params": {"seeds": SEEDS, 
                 "acquisition": "{ name : %s, params : {'kappa': %s}}" % (ACQUISITION, KAPPA), 
@@ -29,13 +29,12 @@ def write_config():
 
     d["search_space"] = {
         "l2_reg": {"min": "1e-7", "max": "1e1", "type": "float", "warp": "log"},
-        "learning_rate": {"min": "1e-3", "max": "1e1", "type": "float", "warp": "log"},
-        "iterations": {"min": "50", "max": "10000", "type": "int", "warp": "log"},
+        "learning_rate": {"min": "1e-3", "max": "1e1", "type": "float", "warp": "log"}
         }
-    #if STRATEGY == "hyperopt_tpe":
-    #    d["search_space"]["n_hidden"] = {"min": "1", "max": "300", "type": "int"}
-    #else:
-    #    d["search_space"]["n_hidden"] = {"min": "1", "max": "300", "type": "int", "warp": "log"}
+    if STRATEGY == "hyperopt_tpe":
+        d["search_space"]["iterations"] = {"min": "50", "max": "10000", "type": "int"}
+    else:
+        d["search_space"]["iterations"] = {"min": "50", "max": "10000", "type": "int", "warp": "log"}
 
     d["random_seed"] = "42"
     d["cv"] = {"name": "kfold", "params": {"n_splits": "9", "shuffle": "False"}}
@@ -53,6 +52,7 @@ if __name__ == "__main__":
     NUM_INIT = None
     SOBOL_INIT = None
     ACQUISITION = None
+    OPTIMIZE_BEST = None
     COUNTER = 1
 
     STRATEGY = "sobol"
@@ -69,15 +69,22 @@ if __name__ == "__main__":
                 continue
             if ACQUISITION == "ucb" and KAPPA == None:
                 continue
-            for NUM_ITER in ["1", "10", "100"]:
-                for NUM_INIT in ["1", "10", "100"]:
-                    for SOBOL_INIT in ["True", "False"]:
-                        if SOBOL_INIT == "True" and NUM_ITER == "1":
-                            continue
-                        COUNTER += 1
-                        #if ACQUISITION == "ucb":
-                        #    print(KAPPA, COUNTER)
-                        write_config()
+
+            for OPTIMIZE_BEST in ["True", "False"]:
+                if OPTIMIZE_BEST == "True" and ACQUISITION not in ["ei", "lars"]:
+                    continue
+
+                for NUM_ITER in ["1", "10", "100"]:
+                    if NUM_ITER == "1" and OPTIMIZE_BEST == "True":
+                        continue
+                    for NUM_INIT in ["1", "10", "100"]:
+                        for SOBOL_INIT in ["True", "False"]:
+                            if SOBOL_INIT == "True" and NUM_ITER == "1":
+                                continue
+                            COUNTER += 1
+                            #if ACQUISITION == "ucb":
+                            #    print(KAPPA, COUNTER)
+                            write_config()
 
 
 
