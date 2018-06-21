201cbc7f50da0a43a17e02485dfc4e02e40ba43e
larsbratholm <larsbratholm@gmail.com>
2018-06-21 17:19:26 +0100

    daily

--- run_models.py
+++ run_models.py
@@ -6,6 +6,7 @@ Generate data for paper
 import os, sys
 import pandas as pd
 import numpy as np
+import sklearn
 import sklearn.model_selection
 
 # Add the module to source path
@@ -15,11 +16,24 @@ sys.path.append(source_path)
 
 from portfolio.model import SingleMethod, NN
 
-def run_SingleMethod(x,y, cost = None, names = None):
-    m = SingleMethod(loss = 'mae')
-    score = outer_cv(x, y, m)
-    print("SingleMethod score:", score, loss, scoring_function)
-    return score
+def run_SingleMethod(x,y, cost = None, names = None, classes = None):
+    if os.path.isfile("pickles/single_method_results.pkl"):
+        return
+    
+    m = SingleMethod()
+    params = {'loss': ('mae', 'rmsd', 'max')}
+    # Separate different classes
+    unique_classes = np.unique(classes)
+    for c in unique_classes:
+        idx = np.where(classes == c)[0]
+    errors, best_cv_params, cv_portfolios, final_portfolio, final_params = \
+            outer_cv(x[idx], y[idx], m, params)
+
+    d = {'errors': errors,
+         'cv_params': best_cv_params,
+         'cv_portfolios': cv_portfolios,
+         'final_portfolio': final_portfolio,
+         'final_params': final_params}
 
 def run_LinearModel(x,y, cost = None, names = None):
     m = NN(tensorboard_dir = '', learning_rate = 1e-1, iterations = 10000, 
@@ -66,33 +80,79 @@ def parse_reaction_dataframe(df):
 
     return energies, reference, cost, names, classes
 
-# TODO this can be done with a pipeline
-def outer_cv(x, y, m):
+def outer_cv(x, y, m, params, grid = True):
     """
     Do outer cross validation to get the prediction errors of a method. 
-    kwargs are a dictionary with options to the Portfolio class.
     """
 
-    #from sklearn.model_selection import cross_val_score, cross_validate
-    #scores = cross_validate(m, x, y, cv=3)
-    #print(scores)
+    if grid:
+        cv_model = sklearn.model_selection.GridSearchCV
+    else:
+        cv_model = sklearn.model_selection.RandomizedSearchCV
 
-    #quit()
+    outer_cv_splits = 5
+    outer_cv_repeats = 1
+    inner_cv_splits = 3
+    inner_cv_repeats = 1
 
-    cv_generator = sklearn.model_selection.RepeatedKFold(n_splits = 5, n_repeats = 10000)
+    outer_cv_generator = sklearn.model_selection.RepeatedKFold(
+            n_splits = outer_cv_splits, n_repeats = outer_cv_repeats)
+    inner_cv_generator = sklearn.model_selection.RepeatedKFold(
+            n_splits = inner_cv_splits, n_repeats = inner_cv_repeats)
 
-    errors = []
-    for train_idx, test_idx in cv_generator.split(y):
+    best_cv_params = []
+    cv_portfolios = []
+    errors = np.zeros((x.shape[0], outer_cv_repeats))
+
+    for i, (train_idx, test_idx) in enumerate(outer_cv_generator.split(y)):
         train_x, train_y = x[train_idx], y[train_idx]
         test_x, test_y = x[test_idx], y[test_idx]
+        cvmod = cv_model(m, param_grid = params, scoring = 'neg_mean_absolute_error',
+                return_train_score = False, cv = inner_cv_generator)
+        cvmod.fit(train_x, train_y)
+        cv_portfolios.append(cvmod.best_estimator_.portfolio)
+        best_cv_params.append(cvmod.best_params_)
+        y_pred = cvmod.predict(test_x)
+        errors[test_idx, i // outer_cv_splits] = test_y - y_pred
 
-        m.fit(train_x, train_y)
-        errors.append(m.score(test_x, test_y))
+    best_params = get_best_params(best_cv_params)
 
-    return np.mean(errors)
+    m.set_params(**best_params)
+    m.fit(x, y)
+    final_portfolio = m.portfolio
+
+    return errors, best_cv_params, cv_portfolios, final_portfolio, best_params
+
+def get_best_params(params):
+    """
+    Attempts to get the best set of parameters
+    from a list of dictionaries containing the
+    parameters that resulted in the best cv score
+    """
+
+    # Preprocess a bit
+    d = {}
+    for param in params:
+        for key,value in param.items():
+            if key not in d: d[key] = []
+            d[key].append(value)
+
+    # Select the most likely or median value
+    for key,value in d.items():
+        # if list choose most common
+        if isinstance(value[0], str):
+            best_value = max(set(value), key=value.count)
+            d[key] = best_value
+            continue
+
+        # if numeric return median
+        d[key] = np.median(value)
+
+    return d
+
 
 if __name__ == "__main__":
-    df = pd.read_pickle(sys.argv[1])
+    df = pd.read_pickle("pickles/abde12_reac.pkl")
     # all but x,y is optional
     x, y, cost, names, rclass = parse_reaction_dataframe(df)
 
@@ -104,7 +164,7 @@ if __name__ == "__main__":
     #out = df[df.reaction == df.iloc[df.time.idxmax()].reaction].values[p][:,[0,3,-2]]
     #print(np.concatenate([out,m.portfolio[p,None]], axis=1))
 
-    run_SingleMethod(x,y)
+    run_SingleMethod(x,y, cost, names, rclass)
     #run_linear(x,y, cost, names, None)
 
     #def __init__(self, learning_rate = 0.3, iterations = 5000, cost_reg = 0.0, l2_reg = 0.0, 
